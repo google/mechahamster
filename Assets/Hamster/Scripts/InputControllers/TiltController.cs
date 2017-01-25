d6ca812c0dd9b6ed599b74045e53b15d28c1c3cc
Chris Cornell <ccornell@google.com>
2017-01-24 19:36:16 -0800

    Changes tilt controls to be better on mobile.
    Before, the device had to be held perfectly flat to
    register as neutral.  Now, the "base" position is set
    when the player is spawned.
    
    In addition, the "base" position will also slowly adjust during
    the game towards the current position, if it is tilted too far
    out of position.
    
    Change-Id: I86983a45b2b4375b0993db3a73ce4bcc59d0fe6a

--- Assets/Hamster/Scripts/InputControllers/TiltController.cs
+++ Assets/Hamster/Scripts/InputControllers/TiltController.cs
@@ -24,12 +24,48 @@ namespace Hamster.InputControllers {
     // Scalar, for modifying the x/z components of the accelerometer
     // velocity, to generate the final player velocity from tilting
     // the device.
-    const float kTiltVelocity = 16.0f;
+    const float TiltVelocity = 16.0f;
 
+    // How fast the down-vector moves, when we're recalibrating.
+    const float RecenterSpeed = 0.9f;
+
+    // How far the down-vector can be off from the input (in radians)
+    // before we start recalibrating.
+    const float MaxAngleDifference = Mathf.PI / 6.0f;  // 30 degrees.
+    static float MaxAngleSinSquared =
+      Mathf.Sin(MaxAngleDifference) * Mathf.Sin(MaxAngleDifference);
+
+    // TODO(ccornell) move these constants into remote-config.
+
+    Vector3 currentDownVector;
+    public TiltController() {
+      currentDownVector = Input.acceleration.normalized;
+      currentDownVector.x = 0;
+    }
 
     public override Vector2 GetInputVector() {
-      Vector3 tiltInput = Input.acceleration.normalized * kTiltVelocity;
-      return new Vector3(tiltInput.x, tiltInput.y);
+      Vector3 inputVector = Input.acceleration.normalized;
+      // For our down-vector, (and the self-adjustments) we only care
+      // about the Y and Z axis.  We don't self-center for the X axis,
+      // since we can assume that the phone will always be held perpendicular
+      // to the face.
+      Vector3 newDownVector = new Vector3(0.0f, inputVector.y, inputVector.z);
+
+      Quaternion fromDownToWorld = Quaternion.FromToRotation(currentDownVector, Vector3.down);
+
+      Vector3 translatedInput = fromDownToWorld * inputVector;
+
+      // If the current angle is too far from the base state, recenter.
+      if (Mathf.Abs(
+        Vector3.Cross(newDownVector, currentDownVector).sqrMagnitude) > MaxAngleSinSquared ||
+        Vector3.Dot(newDownVector, currentDownVector) < 0) {
+        currentDownVector =
+          Vector3.Lerp(currentDownVector, newDownVector,
+            RecenterSpeed * Time.deltaTime).normalized;
+      }
+
+      Vector3 tiltInput = translatedInput * TiltVelocity;
+      return new Vector3(tiltInput.x, -tiltInput.z);
     }
   }
 }
