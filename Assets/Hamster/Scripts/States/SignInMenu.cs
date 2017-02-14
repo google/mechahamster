ef42ccb586a28997215b17406143903afb3c8099
Chris Cornell <ccornell@google.com>
2017-02-13 16:39:54 -0800

    Merged VR and regular scenes.
    Combined the scene for VR with the scene for regular mode,
    into one large scene.  Added more robust branching logic to
    handle this, rather than relying on build settings.
    
    As part of this, also removed the original MainScene, and
    replaced it with the (heavily modified) MainScene_DayDream
    scene.
    
    Also, fixed a bug in which daydream mode would hang if launched
    on devices that hadn't yet signed in in some way.
    
    Change-Id: Id87ee5e13906b6800fd6f7905fdc79833f5c2942

--- Assets/Hamster/Scripts/States/SignInMenu.cs
+++ Assets/Hamster/Scripts/States/SignInMenu.cs
@@ -54,6 +54,13 @@ namespace Hamster.States {
     // TODO(ccornell): This needs some layout attention, if it's going
     // to see much use.  Needs to be centered at a minimum.
     public override void OnGUI() {
+      bool inVrMode = false;
+      // TODO(ccornell) - DAYDREAM SCAFFOLDING
+      VRSystemSetup vrSetup = GameObject.FindObjectOfType<VRSystemSetup>();
+      if (vrSetup) {
+        inVrMode = vrSetup.StartInVRMode;
+      }
+
       GUI.skin = CommonData.prefabs.guiSkin;
       GUILayout.BeginVertical();
       if (GUILayout.Button(StringConstants.ButtonSignInWithEmail)) {
@@ -62,7 +69,7 @@ namespace Hamster.States {
       if (GUILayout.Button(StringConstants.ButtonCreateAccount)) {
         manager.PushState(new CreateAccount());
       }
-      if (GUILayout.Button(StringConstants.ButtonSignInAnonymously)) {
+      if (GUILayout.Button(StringConstants.ButtonSignInAnonymously) || inVrMode) {
         manager.PushState(
             new WaitForTask(auth.SignInAnonymouslyAsync(),
               StringConstants.LabelSigningIn, true));
---
+++ Assets/Hamster/Scripts/VRSystemSetup.cs
@@ -0,0 +1,48 @@
+﻿// Copyright 2017 Google Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using UnityEngine;
+using System.Collections.Generic;
+
+namespace Hamster {
+
+  // This class creates things at startup that are required
+  // by the game
+  public class VRSystemSetup : MonoBehaviour {
+    public bool StartInVRMode = false;
+
+    public GameObject VREventSystem;
+    public GameObject VRViewer;
+    public GameObject VRController;
+    public GameObject VRControllerPointer;
+
+    public CameraController CameraHolder;
+
+    private void Awake() {
+      if (StartInVRMode) {
+        Instantiate(VREventSystem);
+        GameObject viewer = Instantiate(VRViewer);
+        Instantiate(VRController);
+        GvrViewer gvrViewer = viewer.GetComponent<GvrViewer>();
+        if (gvrViewer) {
+          gvrViewer.VRModeEnabled = false;
+        }
+
+        GameObject pointer = Instantiate(VRControllerPointer);
+        pointer.transform.SetParent(CameraHolder.transform);
+        pointer.transform.localPosition = new Vector3(0.0f, 0.0f, -9.75f);
+      }
+    }
+  }
+}
