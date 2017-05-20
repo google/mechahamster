39c7fe7ee8f4b452420cae7296445e9fb0d086dc
Chris Cornell <ccornell@google.com>
2017-05-19 18:29:41 -0700

    Made floor tiles automatically rotate when spawned.
    Floor tiles will now pick a random (90-degree multiple)
    angle when spawned, to make levels look more visually varied.
    
    Change-Id: I2aa2fcd39145635aeba94212022623f479028f56

---
+++ Assets/Hamster/Scripts/Utilities/RandomRotation.cs
@@ -0,0 +1,27 @@
+﻿// Copyright 2017 Google Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using UnityEngine;
+using System.Collections;
+
+namespace Hamster.MapObjects {
+
+  // Script to automatically rotate an object to a random 90
+  // degree angle multiple on spawn.
+  public class RandomRotation : MonoBehaviour {
+    private void Start() {
+      GetComponent<Transform>().transform.Rotate(0, Random.Range(0, 4) * 90.0f, 0);
+    }
+  }
+}
