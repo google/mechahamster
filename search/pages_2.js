76da7ef2d29590d305f7a9629cd8509e5f0c3b9f
David Airapetyan <davidair@google.com>
2018-03-19 18:45:16 -0700

    Updating MechaHamster documentation to include TestLabs and GPGS integration.
    Change-Id: Ic0b03c7513ab0fba150f9cb3841e5ff11ef8edc2

--- search/pages_2.js
+++ search/pages_2.js
@@ -5,5 +5,6 @@ var searchData=
   ['firebase_20realtime_20database',['Firebase Realtime Database',['../mechahamster_guide_database.html',1,'']]],
   ['firebase_20invites',['Firebase Invites',['../mechahamster_guide_invites.html',1,'']]],
   ['firebase_20cloud_20messaging',['Firebase Cloud Messaging',['../mechahamster_guide_messaging.html',1,'']]],
-  ['firebase_20remote_20config',['Firebase Remote Config',['../mechahamster_guide_remoteconfig.html',1,'']]]
+  ['firebase_20remote_20config',['Firebase Remote Config',['../mechahamster_guide_remoteconfig.html',1,'']]],
+  ['firebase_20test_20lab',['Firebase Test Lab',['../mechahamster_guide_testlab.html',1,'']]]
 ];
