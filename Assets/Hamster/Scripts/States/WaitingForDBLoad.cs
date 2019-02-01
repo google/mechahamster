google/mechahamster

commit caea70dba83b3e0914cbc704b9a405c2734de0ca

Cleanup WaitingForDBLoad/WaitingForDBString states
Change-Id: I9debd695c1c1ca3c50f4a2326a9a0e3455f479a6

Assets/Hamster/Scripts/States/WaitingForDBLoad.cs
@@ -12,28 +12,21 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+using System;
+using Firebase.Database;
 using UnityEngine;
-using System.Collections;
-using System.Collections.Generic;
-using Firebase.Unity.Editor;
-
 
 namespace Hamster.States {
   // Utility state, for fetching structures from the database.
   // Returns the result in the result struct.
-  class WaitingForDBLoad<T> : BaseState {
+  class WaitingForDBLoad<T> : BaseState where T : class {
 
-    protected bool isComplete = false;
-    protected bool wasSuccessful = false;
-    protected T result = default(T);
-    protected string path;
-    protected int failedFetches = 0;
-
-    const int MaxDatabaseRetries = 5;
     const float TimeoutSeconds = 10.0f;
 
-    protected Firebase.Database.FirebaseDatabase database;
-
+    T result;
+    string path;
+    bool isComplete;
+    FirebaseDatabase database;
     Menus.SingleLabelGUI menuComponent;
     float timeoutTime;
 
@@ -44,26 +37,21 @@ namespace Hamster.States {
     // Initialization method.  Called after the state
     // is added to the stack.
     public override void Initialize() {
-      database = Firebase.Database.FirebaseDatabase.GetInstance(CommonData.app);
+      database = FirebaseDatabase.GetInstance(CommonData.app);
       database.GetReference(path).ValueChanged += HandleResult;
-
       menuComponent = SpawnUI<Menus.SingleLabelGUI>(StringConstants.PrefabsSingleLabelMenu);
-      UpdateLabelText();
       timeoutTime = Time.realtimeSinceStartup + TimeoutSeconds;
     }
 
-    protected virtual void HandleResult(object sender,
-      Firebase.Database.ValueChangedEventArgs args) {
-
+    void HandleResult(object sender, ValueChangedEventArgs args) {
       if (args.DatabaseError != null) {
         Debug.LogError("Database error :" + args.DatabaseError.Code + ":\n" +
           args.DatabaseError.Message + "\n" + args.DatabaseError.Details);
       } else {
-        wasSuccessful = true;
         if (args.Snapshot != null) {
-          string json = args.Snapshot.GetRawJsonValue();
+          var json = args.Snapshot.GetRawJsonValue();
           if (!string.IsNullOrEmpty(json)) {
-            result = JsonUtility.FromJson<T>(json);
+            result = ParseResult(json);
           }
         }
       }
@@ -79,7 +67,11 @@ namespace Hamster.States {
       }
     }
 
-    private void UpdateLabelText() {
+    protected virtual T ParseResult(string json) {
+      return JsonUtility.FromJson<T>(json);
+    }
+    
+    void UpdateLabelText() {
       if (menuComponent != null) {
         menuComponent.LabelText.text =
           StringConstants.LabelLoading + Utilities.StringHelper.CycleDots();
@@ -90,7 +82,7 @@ namespace Hamster.States {
       database.GetReference(path).ValueChanged -= HandleResult;
       DestroyUI();
       return new StateExitValue(
-        typeof(WaitingForDBLoad<T>), new Results(path, result, wasSuccessful));
+        typeof(WaitingForDBLoad<T>), new Results(path, result, result != null));
     }
 
     // Class for encapsulating the results of the database load, as

Assets/Hamster/Scripts/States/WaitingForDBString.cs
@@ -11,35 +11,17 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-using UnityEngine;
-using System.Collections;
-using System.Collections.Generic;
-using Firebase.Unity.Editor;
 namespace Hamster.States {
   // Utility state, for fetching strings.  Is basically
   // just a specialization of WaitingForDBLoad, but it needs
   // some minor changes to how the results are parsed, since
   // they're not technically valid json.
   class WaitingForDBString : WaitingForDBLoad<string> {
-    public WaitingForDBString(string path) : base(path) { }
-    protected override void HandleResult(object sender,
-      Firebase.Database.ValueChangedEventArgs args) {
-      // Remove the listener as soon as we get a response.
-      database.GetReference(path).ValueChanged -= HandleResult;
-      if (args.DatabaseError != null) {
-        Debug.LogError("Database error :" + args.DatabaseError.Code + ":\n" +
-          args.DatabaseError.Message + "\n" + args.DatabaseError.Details);
-      } else {
-        wasSuccessful = true;
-        if (args.Snapshot != null) {
-          string json = args.Snapshot.GetRawJsonValue();
-          if (json.Length > 2) {
-            result = json.Substring(1, json.Length - 2);
-            wasSuccessful = true;
-          }
-        }
-      }
-      isComplete = true;
+
+    public WaitingForDBString(string path) : base (path) { }
+
+    protected override string ParseResult(string json) {
+      return json.Length > 2 ? json.Substring (1, json.Length - 2) : null;
     }
   }
 }

commit 4ce7decee8318a810d1a0f0b1e59895edf00fd46

WaitingForDBLoad returns successful even if the data is empty.
This is a regression from caea70dba83b3e0914cbc704b9a405c2734de0ca.
Fix it so that the user can fetch and create user data right after
the first sign-in.

Change-Id: Id75b83e55bc3b12e8e97352edd2ad0ad0e52a9c1

Assets/Hamster/Scripts/States/WaitingForDBLoad.cs
@@ -23,9 +23,10 @@ namespace Hamster.States {
 
     const float TimeoutSeconds = 10.0f;
 
-    T result;
+    T result = default(T);
     string path;
-    bool isComplete;
+    bool isComplete = false;
+    bool wasSuccessful = false;
     FirebaseDatabase database;
     Menus.SingleLabelGUI menuComponent;
     float timeoutTime;
@@ -48,6 +49,9 @@ namespace Hamster.States {
         Debug.LogError("Database error :" + args.DatabaseError.Code + ":\n" +
           args.DatabaseError.Message + "\n" + args.DatabaseError.Details);
       } else {
+        // It is considered as successful as long as there is no error, even the data at the
+        // given path is empty (null).
+        wasSuccessful = true;
         if (args.Snapshot != null) {
           var json = args.Snapshot.GetRawJsonValue();
           if (!string.IsNullOrEmpty(json)) {
@@ -70,7 +74,7 @@ namespace Hamster.States {
     protected virtual T ParseResult(string json) {
       return JsonUtility.FromJson<T>(json);
     }
-    
+
     void UpdateLabelText() {
       if (menuComponent != null) {
         menuComponent.LabelText.text =
@@ -82,7 +86,7 @@ namespace Hamster.States {
       database.GetReference(path).ValueChanged -= HandleResult;
       DestroyUI();
       return new StateExitValue(
-        typeof(WaitingForDBLoad<T>), new Results(path, result, result != null));
+        typeof(WaitingForDBLoad<T>), new Results(path, result, wasSuccessful));
     }
 
     // Class for encapsulating the results of the database load, as